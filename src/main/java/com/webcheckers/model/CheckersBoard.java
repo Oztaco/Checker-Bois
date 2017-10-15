397795fdfc4338731f19def4527c17efd09ad01a
Jon <jrh4099@g.rit.edu>
2017-10-15 11:42:10 -0400

    Implemented initBoard to fill the board with pieces and spaces. Probably a more efficient way to do it, but it should work for now. Also added printBoard to print the entire board to the console, to ensure that it looks as it should.

--- src/main/java/com/webcheckers/model/CheckersBoard.java
+++ src/main/java/com/webcheckers/model/CheckersBoard.java
@@ -16,4 +16,100 @@ public class CheckersBoard {
     private Player player1;
     private Player player2;
 
+    /**
+     * Initializes the board, filling it with enums
+     * corresponding to the contents of the spaces.
+     */
+    private void initBoard(){
+        for(int y = 0; y < 8; y++){
+            for(int x = 0; x < 8; x++){
+                //player 1 side
+                if(y == 0 || y == 2){
+                    if(x % 2 == 0){
+                        board[y][x] = space.INVALID;
+                    }
+                    else{
+                        board[y][x] = space.PLAYER1;
+                    }
+                }
+                else if(y == 1){
+                    if(x % 2 == 0){
+                        board[y][x] = space.PLAYER1;
+                    }
+                    else{
+                        board[y][x] = space.INVALID;
+                    }
+                }
+
+                //middle area
+                else if(y == 3){
+                    if(x % 2 == 0){
+                        board[y][x] = space.EMPTY;
+                    }
+                    else{
+                        board[y][x] = space.INVALID;
+                    }
+                }
+                else if(y == 4){
+                    if(x % 2 == 0){
+                        board[y][x] = space.INVALID;
+                    }
+                    else{
+                        board[y][x] = space.EMPTY;
+                    }
+                }
+
+                //player 2 side
+                if(y == 5 || y == 7){
+                    if(x % 2 == 0){
+                        board[y][x] = space.PLAYER2;
+                    }
+                    else{
+                        board[y][x] = space.INVALID;
+                    }
+                }
+                else{   //y == 6
+                    if(x % 2 == 0){
+                        board[y][x] = space.INVALID;
+                    }
+                    else{
+                        board[y][x] = space.PLAYER2;
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * Prints the board for testing purposes.
+     */
+    private void printBoard(){
+        System.out.println("");
+        String val;
+
+        for(int y = 0; y < 8; y++) {
+            for (int x = 0; x < 8; x++) {
+                if(board[y][x] == space.INVALID){
+                    val = "X";
+                }
+                else if(board[y][x] == space.EMPTY){
+                    val = " ";
+                }
+                else if(board[y][x] == space.PLAYER1){
+                    val = "1";
+                }
+                else if(board[y][x] == space.PLAYER2){
+                    val = "2";
+                }
+                else if(board[y][x] == space.PLAYER1KING){
+                    val = "A";
+                }
+                else{
+                    val = "B";
+                }
+                System.out.print("[" + val + "]");
+            }
+        }
+        System.out.println("");
+    }
 }
