6fd72dc1f4575e1b80190823a8317f428d4bd7ad
Andrew Shin <as6323@rit.edu>
2017-10-16 03:34:19 -0400

    Changes to player

--- src/main/java/com/webcheckers/model/Player.java
+++ src/main/java/com/webcheckers/model/Player.java
@@ -1,16 +1,31 @@
 package com.webcheckers.model;
 
 public class Player{
+    private String name;
+
     private int pieces;
     private int kings;
+
+    public Player(String name) {
+        this.pieces = 12;
+        this.kings = 0;
+        this.name = name;
+    }
+
 
     /**
      * Sets up a player to play the game with 12 pieces and 0 kings
      */
-    public void setupPlayer(){
+    public void setupPlayer(String playerName){
         this.pieces = 12;
         this.kings = 0;
+        this.name = playerName;
     }
+
+    public String getName(){
+        return name;
+    }
+
     /**
      * Kings a single piece
      */
