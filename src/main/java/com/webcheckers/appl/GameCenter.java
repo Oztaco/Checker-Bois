48c41a09ec28a4883bfbfe6ae4fc2c540324df46
Efe Ozturkoglu <efeecehanozturkoglu@yahoo.com>
2017-10-16 04:50:48 -0400

    Added support for sessions

--- src/main/java/com/webcheckers/appl/GameCenter.java
+++ src/main/java/com/webcheckers/appl/GameCenter.java
@@ -2,13 +2,15 @@ package com.webcheckers.appl;
 
 import java.util.logging.Logger;
 
-import com.example.model.GuessGame;
+import Java.util.ArrayList;
+import com.webcheckers.appl.PlayerSession;
+
 
 /**
  * The class that centralizes communication within the server,
  * glueing the model and the UI together and maintaining state.
  *
- * @author <a href='mailto:bdbvse@rit.edu'>Efe Ozturkoglu</a>
+ * @author <a href='mailto:exo3392@rit.edu'>Efe Ozturkoglu</a>
  */
 public class GameCenter {
   private static final Logger LOG = Logger.getLogger(GameCenter.class.getName());
@@ -23,59 +25,30 @@ public class GameCenter {
   // Attributes
   //
 
+  ArrayList<PlayerSession> playerSessions = new ArrayList<PlayerSession>();
+
   //
   // Constructors
   //
+
+  public GameCenter() {
+
+  }
 
   //
   // Public methods
   //
 
-  /**
-   * Get a new {@Linkplain PlayerServices} object to provide client-specific services to
-   * the client who just connected to this application.
-   *
-   * @return
-   *   A new {@Link PlayerServices}
-   */
-  public PlayerServices newPlayerServices() {
-    LOG.fine("New player services instance created.");
-    return new PlayerServices(this);
+  public void addSession(String _username, String _ip) {
+    playerSessions.add(new PlayerSession(_username, _ip));
   }
-
-  /**
-   * Create a new {@Linkplain GuessGame} game.
-   *
-   * @return
-   *   A new {@link GuessGame}
-   */
-  public GuessGame getGame() {
-    return new GuessGame(this);
-  }
-
-  /**
-   * Collect sitewide statistics when a game is finished.
-   */
-  public void gameFinished() {
-    // do some application-wide book-keeping
-    synchronized (this) {  // protect the critical code
-      totalGames++;
+  
+  public PlayerSession getSessionByIp(String _ip) {
+    for (PlayerSession p : playerSessions) {
+      if (p.getIp() == _ip) {
+        return p;
+      }
     }
   }
 
-  /**
-   * Get a user message about the sitewide statistics.
-   *
-   * @return
-   *   The message to the user about global game statistics.
-   */
-  public synchronized String getGameStatsMessage() {
-    if (totalGames > 1) {
-      return String.format(GAMES_PLAYED_FORMAT, totalGames);
-    } else if (totalGames == 1) {
-      return ONE_GAME_MESSAGE;
-    } else {
-      return NO_GAMES_MESSAGE;
-    }
-  }
 }
