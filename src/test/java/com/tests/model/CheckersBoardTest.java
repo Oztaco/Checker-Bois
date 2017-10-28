a8a3bc47d8e3d9e7abf8a5b6d6ad8daaa6ab490e
Jon <jrh4099@g.rit.edu>
2017-10-28 19:21:22 -0400

    Added 2 tests, one that checks if the CheckersBoard constructor runs without failure, the other checking if the PlayerSession constructor runs without failure.

--- src/test/java/com/tests/model/CheckersBoardTest.java
+++ src/test/java/com/tests/model/CheckersBoardTest.java
@@ -1,14 +1,27 @@
 package com.tests.model;
 
+import com.webcheckers.appl.PlayerSession;
 import com.webcheckers.model.CheckersBoard;
 import com.webcheckers.model.InvalidMoveException;
 import com.webcheckers.model.Player;
 import org.junit.Test;
 
+import static org.mockito.Mockito.mock;
+
 //import static org.junit.jupiter.api.Assertions.*; //TODO
 
 //<<<<<<< Updated upstream //TODO
 class CheckersBoardTest {
+    @Test
+    private void testBoardConstructor(){
+        new CheckersBoard(new Player("player1"),(new Player("player2")));
+    }
+
+    @Test
+    private void testPlayerSessionConstructor(){
+        new PlayerSession("username","1234");
+    }
+
     @Test(expected = InvalidMoveException.class)
     private void testMoveNotRightPlayer() throws InvalidMoveException {
         CheckersBoard cb2 = new CheckersBoard(new Player("Fluffy"), new Player("Fatty"));
@@ -16,6 +29,7 @@ class CheckersBoardTest {
         cb2.printBoard();
         cb2.move(1, 2, 0, 3, cb2.getPlayer(2), CheckersBoard.moveType.move);
     }
+
     @Test
     private void testValidMove() throws InvalidMoveException {
         CheckersBoard cb1 = new CheckersBoard(new Player("Fluffy"), new Player("Fatty"));
