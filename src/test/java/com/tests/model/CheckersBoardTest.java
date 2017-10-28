904cdc414bdab1f6a3b4c9eeb191def2cc8b858f
cmrnh <ceh7606@rit.edu>
2017-10-28 00:39:35 -0400

    There's an error but I set up the first unit test and packages for all other ones

---
+++ src/test/java/com/tests/model/CheckersBoardTest.java
@@ -0,0 +1,12 @@
+package com.tests.model;
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CheckersBoardTest {
+    @org.junit.Test
+    public void move() throws Exception {
+    }
+
+}
