b908fe0e7034d7904c5fbf138ceedab6d6a02008
Jon <jrh4099@g.rit.edu>
2017-10-28 22:41:46 -0400

    Added PlayerSessionTest and moved one of the test methods after learning how the tests should be laid out.

--- src/test/java/com/tests/model/CheckersBoardTest.java
+++ src/test/java/com/tests/model/CheckersBoardTest.java
@@ -1,12 +1,9 @@
 package com.tests.model;
 
-import com.webcheckers.appl.PlayerSession;
 import com.webcheckers.model.CheckersBoard;
 import com.webcheckers.model.InvalidMoveException;
 import com.webcheckers.model.Player;
 import org.junit.Test;
-
-import static org.mockito.Mockito.mock;
 
 //import static org.junit.jupiter.api.Assertions.*; //TODO
 
@@ -15,11 +12,6 @@ class CheckersBoardTest {
     @Test
     private void testBoardConstructor(){
         new CheckersBoard(new Player("player1"),(new Player("player2")));
-    }
-
-    @Test
-    private void testPlayerSessionConstructor(){
-        new PlayerSession("username","1234");
     }
 
     @Test(expected = InvalidMoveException.class)
