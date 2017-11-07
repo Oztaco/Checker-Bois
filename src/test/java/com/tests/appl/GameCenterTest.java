2e7e88a534f4b78db624e6024457f6c7c572952a
Jon <jrh4099@g.rit.edu>
2017-11-07 11:34:43 -0500

    Implemented GameCenterTest and LobbyTest. Fixed typo in PlayerTest.

--- src/test/java/com/tests/appl/GameCenterTest.java
+++ src/test/java/com/tests/appl/GameCenterTest.java
@@ -1,4 +1,22 @@
 package com.tests.appl;
 
+import com.webcheckers.appl.GameCenter;
+import org.junit.Test;
+
 public class GameCenterTest {
+
+    @Test
+    public void testAddSession(){
+        GameCenter gc = new GameCenter();
+        gc.addSession("test", "1337");
+    }
+
+    @Test
+    public void testGetSession(){
+        GameCenter gc = new GameCenter();
+        gc.addSession("test", "1337");
+        if(gc.getSessionByIp("1337") == null){
+            System.out.println("ERROR: IP 1337 not found in GameCenter");
+        }
+    }
 }
--- src/test/java/com/tests/model/LobbyTest.java
+++ src/test/java/com/tests/model/LobbyTest.java
@@ -1,4 +1,67 @@
 package com.tests.model;
 
+import com.webcheckers.model.CheckersBoard;
+import com.webcheckers.model.Exceptions.GameNotAddedException;
+import com.webcheckers.model.Exceptions.PlayerNotAddedException;
+import com.webcheckers.model.Lobby;
+import com.webcheckers.model.Player;
+import org.junit.Test;
+
 public class LobbyTest {
+
+    @Test
+    public void testLobbyConstructor(){
+        Lobby l = new Lobby();
+    }
+
+    @Test
+    public void testAddPlayer(){
+        Lobby l = new Lobby();
+        Player p = new Player("testy");
+        try{
+            l.addPlayer(p);
+        }
+        catch (PlayerNotAddedException e){
+            System.out.println("ERROR: Player was not added to lobby");
+        }
+    }
+
+    @Test
+    public void testAddGame(){
+        Lobby l = new Lobby();
+        Player p1 = new Player("testy");
+        Player p2 = new Player("testo");
+        CheckersBoard cb = new CheckersBoard(p1,p2);
+        try{
+            l.addPlayer(p1);
+            l.addPlayer(p2);
+            l.addGame(cb);
+        }
+        catch(PlayerNotAddedException e){
+            System.out.println("ERROR: Player was not added to lobby");
+        }
+        catch(GameNotAddedException e){
+            System.out.println("ERROR: Game was not added to lobby");
+        }
+    }
+
+    @Test
+    public void testInvalidAddGame(){
+        Lobby l = new Lobby();
+        Player p1 = new Player("testy");
+        Player p2 = new Player("testo");
+        Player p3 = new Player("bad man");
+        CheckersBoard cb = new CheckersBoard(p1,p2);
+        try{
+            l.addPlayer(p1);
+            l.addPlayer(p3);
+            l.addGame(cb);
+        }
+        catch(PlayerNotAddedException e){
+            System.out.println("ERROR: Player was not added to lobby");
+        }
+        catch(GameNotAddedException e){
+            System.out.println("Success: Lobby did not contain proper players to add game");
+        }
+    }
 }
