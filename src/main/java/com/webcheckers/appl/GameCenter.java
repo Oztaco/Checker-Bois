e8c4a14baf3f073520f2f1693cc3e816fb751343
cmrnh <ceh7606@rit.edu>
2017-11-09 16:40:18 -0500

    Reformatted Lobby, GameCenter

--- src/main/java/com/webcheckers/appl/GameCenter.java
+++ src/main/java/com/webcheckers/appl/GameCenter.java
@@ -21,37 +21,45 @@ public class GameCenter {
     //ArrayList<PlayerSession> playerSessions = new ArrayList<PlayerSession>();
 
 
-    //
+    //##################################################################################################################
     // Constructors
-    //
+    //##################################################################################################################
      public GameCenter() {
          this.lobby = new Lobby();
      }
 
 
-    //
-    // Public methods
-    //
-
+    //##################################################################################################################
+    // Routing methods
+    //##################################################################################################################
     /**
+     * -----------------------------------------------------------------------------------------------------------------
+     * addPlayer(String username)
+     *
      * Routes an Add Player request from the UI to the corresponding Model method
+     * TODO : FORMAT EXCEPTION
      * @param username
+     * -----------------------------------------------------------------------------------------------------------------
      */
     public void addPlayer(String username){
          try {
              this.lobby.addPlayer(username);
          }
          catch(PlayerNotAddedException e){
-             //TODO WHAT DO WE DO
+
          }
     }
 
     /**
+     * -----------------------------------------------------------------------------------------------------------------
+     * addNewGame(String p1, String p2)
+     *
      * Routes an Add Game request from the UI to the corresponding Model Method
      * @param p1
      * @param p2
+     * -----------------------------------------------------------------------------------------------------------------
      */
-    public void addGame(String p1, String p2){
+    public void addNewGame(String p1, String p2){
         try{
             this.lobby.addNewGame(p1,p2);
         }
@@ -61,49 +69,54 @@ public class GameCenter {
     }
 
     /**
+     * -----------------------------------------------------------------------------------------------------------------
+     * getPlayers()
+     *
      * Routes a request from the UI to get a JSON string of the Players list to the proper aspect of the Lobby
-     * @return
+     * @return allPlayers
+     * -----------------------------------------------------------------------------------------------------------------
      */
     public String getPlayers(){
         return lobby.getPlayersAsString();
     }
 
     /**
+     * -----------------------------------------------------------------------------------------------------------------
+     * getAllGames
+     *
      * Routes a request from the UI to get a JSON string of the Map of Games to the proper aspect of the lobby
-     * @return
+     * @return allGames
+     * -----------------------------------------------------------------------------------------------------------------
      */
     public String getAllGames(){
         return lobby.getGamesAsString();
     }
 
+    /**
+     * -----------------------------------------------------------------------------------------------------------------
+     * getAllGamesForPlayer
+     *
+     * Routes a request from the UI to get a string representation of the Games for a single player for use in a JSON
+     * file to the proper aspect of the lobby
+     * @param username
+     * @return gamesForPlayer
+     * -----------------------------------------------------------------------------------------------------------------
+     */
     public String getAllGamesForPlayer(String username){
         return lobby.getGamesAsString(username);
     }
 
     /**
+     * -----------------------------------------------------------------------------------------------------------------
+     * getGame
+     *
      * Routes a request from the UI to get a JSON string of a Game to the proper aspect of the lobby
      * @param id
-     * @return
+     * @param player
+     * @return game
+     * -----------------------------------------------------------------------------------------------------------------
      */
     public String getGame(String id, int player){
         return lobby.getGame(id).getGameBoardJSON(player);
     }
-
-
-    /**
-     public void addSession(String _username, String _ip) {
-     playerSessions.add(new PlayerSession(_username, _ip));
-     }
-     */
-
-    /**
-    public PlayerSession getSessionByIp(String _ip) {
-        for (PlayerSession p : playerSessions) {
-            if (p.getIp() == _ip) {
-                return p;
-            }
-        }
-        return null; //I added this because I needed to compile my code //TODO
-    }
-     */
 }
--- src/main/java/com/webcheckers/model/Lobby.java
+++ src/main/java/com/webcheckers/model/Lobby.java
@@ -13,32 +13,34 @@ public class Lobby {
     private HashMap<String,Player> players;     //Maps Usernames to Player Objects
     private HashMap<String,Game> games;         //Maps IDs to Game Objects
 
-    //
+    //##################################################################################################################
     //Constructor
-    //
+    //##################################################################################################################
     public Lobby(){
         this.games = new HashMap<String,Game>();
         this.players = new HashMap<String,Player>();
     }
 
 
-    //
+    //##################################################################################################################
     //Getters
-    //
+    //##################################################################################################################
     public Game getGame(String id){
         return this.games.get(id);
     }
 
 
-    //
-    //Public Methods
-    //
+    //##################################################################################################################
+    //Lobby Management Methods
+    //##################################################################################################################
     /**
+     * -----------------------------------------------------------------------------------------------------------------
      * addPlayer(Player player)
      *
      * adds a player specified to the lobby.
      * @param username
      * @throws PlayerNotAddedException
+     * -----------------------------------------------------------------------------------------------------------------
      */
     public void addPlayer(String username) throws PlayerNotAddedException {
         try{
@@ -51,11 +53,13 @@ public class Lobby {
     }
 
     /**
+     * -----------------------------------------------------------------------------------------------------------------
      * addGame(CheckersBoard game)
      *
      * Adds game to list of games, given it's players are in the lobby.
      * @param p1, p2
      * @throws GameNotAddedException
+     * -----------------------------------------------------------------------------------------------------------------
      */
     public void addNewGame(String p1, String p2) throws GameNotAddedException{
         String newId = generateID();
@@ -69,11 +73,19 @@ public class Lobby {
         }
     }
 
+
+    //##################################################################################################################
+    //Private Methods
+    //##################################################################################################################
     /**
+     * -----------------------------------------------------------------------------------------------------------------
+     * generateID()
+     *
      * Generates a Random ID to assign to every game.
-     * @return Int randID
+     * @return randID
+     * -----------------------------------------------------------------------------------------------------------------
      */
-    public String generateID(){
+    private String generateID(){
         int range = this.validChars.length()-1;
         String randId = "";
         for(int i = 0; i <= 6; i++){
@@ -88,8 +100,22 @@ public class Lobby {
         return randId;
     }
 
+
+    //##################################################################################################################
+    //PUBLIC METHODS
+    //##################################################################################################################
+
+
+
+    //##################################################################################################################
+    //TESTING METHODS
+    //##################################################################################################################
     /**
+     * -----------------------------------------------------------------------------------------------------------------
+     * printLobby()
+     *
      * Prints lobby for testing purposes
+     * -----------------------------------------------------------------------------------------------------------------
      */
     public void printLobby(){
         System.out.println("Lobby:");
@@ -101,26 +127,45 @@ public class Lobby {
         System.out.println("\tNumber of unique games: " + this.games.keySet().size());
     }
 
+
+    //##################################################################################################################
+    //COMMUNICATION METHODS
+    //##################################################################################################################
     /**
-     * //TODO WRITE THIS WHOLE THING LOL
+     * -----------------------------------------------------------------------------------------------------------------
+     * getPlayersAsString()
+     *
+     * Formats the players in the lobby for use in JSON files
+     * TODO: METHOD STUBBED
      * @return
+     * -----------------------------------------------------------------------------------------------------------------
      */
     public String getPlayersAsString(){
-        //TODO WRITE CODE
         return null;
     }
 
     /**
-     * //TODO WRITE THIS TOO LOL
-     * Getter for games
+     * -----------------------------------------------------------------------------------------------------------------
+     * getGamesAsString()
+     *
+     * Formats all games in a Lobby for use in JSON files
+     * TODO : METHOD STUBBED
      * @return ArrayList<CheckersBoard>
+     * -----------------------------------------------------------------------------------------------------------------
      */
     public String getGamesAsString(){
-        //Jon owes me $5 for wings
-        //TODO WRITE CODE
         return null;
     }
 
+    /**
+     * -----------------------------------------------------------------------------------------------------------------
+     * getGamesAsPlayer
+     *
+     * Formats all games for a specific player as a string for use in a JSON file
+     * @param player
+     * @return playerGames
+     * -----------------------------------------------------------------------------------------------------------------
+     */
     public String getGamesAsString(String player){
         String playerGames = "";
         for(String id : this.players.get(player).getIds()){
@@ -132,7 +177,4 @@ public class Lobby {
         return playerGames;
     }
 
-    public static void main(String args[]){
-
-    }
 }
