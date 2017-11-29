6f0f5cedeb47b11c732c33e75e41c118c0ae6063
Jon <jrh4099@g.rit.edu>
2017-11-28 20:45:17 -0500

    Implemented GameTest based on it's current implementation.

--- src/test/java/com/tests/model/GameTest.java
+++ src/test/java/com/tests/model/GameTest.java
@@ -1,5 +1,10 @@
 package com.tests.model;
 
+import com.webcheckers.model.Game;
+import com.webcheckers.model.MoveType;
+import com.webcheckers.model.Player;
+import org.junit.Test;
+
 /**
  * Unit tests for the Game class.
  *
@@ -7,4 +12,42 @@ package com.tests.model;
  */
 
 public class GameTest {
+    @Test
+    public void testGameConstructor(){
+        Player p1 = new Player("123","testy");
+        Player p2 = new Player("456","testo");
+        Game g = new Game(p1,p2,"1");
+    }
+
+    @Test
+    public void testPlayTurnMove1(){
+        Player p1 = new Player("123","testy");
+        Player p2 = new Player("456","testo");
+        Game g = new Game(p1,p2,"1");
+        g.playTurn(p1,1,2,0,3, MoveType.MOVE);
+    }
+
+    @Test
+    public void testPlayTurnMove2(){
+        Player p1 = new Player("123","testy");
+        Player p2 = new Player("456","testo");
+        Game g = new Game(p1,p2,"1");
+        g.playTurn(p2,0,5,1,4, MoveType.MOVE);
+    }
+
+    @Test
+    public void testPlayTurnAttack1(){
+        Player p1 = new Player("123","testy");
+        Player p2 = new Player("456","testo");
+        Game g = new Game(p1,p2,"1");
+        g.playTurn(p1,3,4,5,6, MoveType.ATTACK);
+    }
+
+    @Test
+    public void testPlayTurnAttack2(){
+        Player p1 = new Player("123","testy");
+        Player p2 = new Player("456","testo");
+        Game g = new Game(p1,p2,"1");
+        g.playTurn(p1,4,5,2,3, MoveType.ATTACK);
+    }
 }
