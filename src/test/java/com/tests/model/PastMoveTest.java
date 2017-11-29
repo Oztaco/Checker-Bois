3c8cfa9145ee136ebf6a4958d65adf802388d1ea
Jon <jrh4099@g.rit.edu>
2017-11-28 21:30:33 -0500

    Added tests for getters in PastMoveTest.

--- src/test/java/com/tests/model/PastMoveTest.java
+++ src/test/java/com/tests/model/PastMoveTest.java
@@ -17,4 +17,46 @@ public class PastMoveTest {
         Player p = new Player("test","johnny");
         new PastMove(0,0,1,1, MoveType.ATTACK,p);
     }
+
+    @Test
+    public void testGetX0(){
+        Player p = new Player("test","johnny");
+        PastMove pm = new PastMove(0,1,2,3, MoveType.MOVE,p);
+        pm.getX0();
+    }
+
+    @Test
+    public void testGetY0(){
+        Player p = new Player("test","johnny");
+        PastMove pm = new PastMove(0,1,2,3, MoveType.MOVE,p);
+        pm.getY0();
+    }
+
+    @Test
+    public void testGetX1(){
+        Player p = new Player("test","johnny");
+        PastMove pm = new PastMove(0,1,2,3, MoveType.MOVE,p);
+        pm.getX1();
+    }
+
+    @Test
+    public void testGetY1(){
+        Player p = new Player("test","johnny");
+        PastMove pm = new PastMove(0,1,2,3, MoveType.MOVE,p);
+        pm.getY1();
+    }
+
+    @Test
+    public void testGetType(){
+        Player p = new Player("test","johnny");
+        PastMove pm = new PastMove(0,1,2,3, MoveType.MOVE,p);
+        pm.getType();
+    }
+
+    @Test
+    public void testGetPlayerPastMove(){
+        Player p = new Player("test","johnny");
+        PastMove pm = new PastMove(0,1,2,3, MoveType.MOVE,p);
+        pm.getPlayerPastMove();
+    }
 }
