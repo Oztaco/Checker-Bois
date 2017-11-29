6a03d9d3c81fffd2cbd8d1b9522df388f4729fb2
Andrew Shin <as6323@rit.edu>
2017-11-29 00:56:46 -0500

    Added checks for blank username and currently logged in

--- src/main/java/com/webcheckers/ui/PostSignInRoute.java
+++ src/main/java/com/webcheckers/ui/PostSignInRoute.java
@@ -60,13 +60,29 @@ public class  PostSignInRoute implements Route {
     LOG.finer(" PostSignInRoute is invoked.");
     LOG.severe(request.queryParams("username"));
 
-    if (request.queryParams("username").contains("\"")) {
-      //response.redirect("/signin");
-
+    if (request.queryParams("username").equals("")) {
       Map<String, Object> vm = new HashMap<>();
       vm.put("title", "Welcome!");
       vm.put("error", true);
-      vm.put("error_message", "Usernames cannot contain \"");
+      vm.put("error_message", "Username should not be empty");
+      return templateEngine.render(new ModelAndView(vm, "home.ftl"));
+    }
+
+    if (request.queryParams("username").contains("\"")) {
+      Map<String, Object> vm = new HashMap<>();
+      vm.put("title", "Welcome!");
+      vm.put("error", true);
+      vm.put("error_message", "Username cannot contain \"");
+      return templateEngine.render(new ModelAndView(vm, "home.ftl"));
+    }
+
+    String playerList = gameCenter.getPlayers();
+
+    if (playerList.contains(request.queryParams("username"))) {
+      Map<String, Object> vm = new HashMap<>();
+      vm.put("title", "Welcome!");
+      vm.put("error", true);
+      vm.put("error_message", "User currently logged in");
       return templateEngine.render(new ModelAndView(vm, "home.ftl"));
     }
 
