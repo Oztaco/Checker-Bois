4f1956558eba110ef88cd2286bb2d2f749357170
Jon <jrh4099@g.rit.edu>
2017-11-28 19:51:22 -0500

    Added two new test files, as well as remove what was in GameCenter. GameCenter will be redone to reflect the new implementation of the class. Fixed a goof with branching.

--- src/test/java/com/tests/appl/GameCenterTest.java
+++ src/test/java/com/tests/appl/GameCenterTest.java
@@ -3,20 +3,6 @@ package com.tests.appl;
 import com.webcheckers.appl.GameCenter;
 import org.junit.Test;
 
-//public class GameCenterTest {
+public class GameCenterTest {
 
-//    @Test
-//    public void testAddSession(){
-//        GameCenter gc = new GameCenter();
-//       gc.addSession("test", "1337");
-//    }
-
-//    @Test
-//    public void testGetSession(){
-//        GameCenter gc = new GameCenter();
-//        gc.addSession("test", "1337");
-//        if(gc.getSessionByIp("1337") == null){
-//            System.out.println("ERROR: IP 1337 not found in GameCenter");
-//        }
-//    }
-//}
+}
